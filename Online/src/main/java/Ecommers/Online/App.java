c73d276b0e9b8b9d92c7171545a9638c7970a824
siva <vikramraj2206@gmail.com>
2018-11-20 20:11:13 +0530

    Git Staging view. Select all the files in the unstaged changes and drag it to the Staged Changes.

---
+++ Online/src/main/java/Ecommers/Online/App.java
@@ -0,0 +1,13 @@
+package Ecommers.Online;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
